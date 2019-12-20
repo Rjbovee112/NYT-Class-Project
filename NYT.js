7203095d0bd57a52519ce21524a249367254deac
Robert Bovee <Rjbovee112@gmail.com>
2019-12-19 20:47:21 -0700

    api read and come back

--- NYT.js
+++ NYT.js
@@ -7,11 +7,28 @@ var endYear = $('#end-year').val();
 var search = $('#run-search').val();
 var clearResults = $('#clear-all').val();
 
+function clear() {
+    $('#articleSection').empty();
+}
 
+
+function buildQueryURL() {
+    var myKey = "IpG44rHfGTHu1tDo8AEfuzr0PT2ZEOds"
+    var searchTerm = $('#search-term').val();
+    var startYear = $('#start-year').val();
+    var endYear = $('#end-year').val();
+    var search = $('#run-search').val();
+    var clearResults = $('#clear-all').val();
+    var nytimes = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+    var url = nytimes + "q=" + searchTerm + "begin_date=" + startYear + "&end_date=" + endYear + "&api_keys=" + myKey;
+    console.log(url)
+    return url;
+}
 
 $('#run-search').on('click', function () {
     event.preventDefault()
-    var search = $('#search-term').val();
+    var url = buildQueryURL()
+    console.log(url)
 })
 
 
