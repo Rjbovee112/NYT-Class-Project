f9cb7fa24d2575282e2adfb2f59f80c753bf5d9c
Robert Bovee <Rjbovee112@gmail.com>
2019-12-19 19:55:19 -0700

    search button working

--- NYT.js
+++ NYT.js
@@ -1,6 +1,18 @@
-var queryURL = 
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=IpG44rHfGTHu1tDo8AEfuzr0PT2ZEOds"
+var apiKey = "IpG44rHfGTHu1tDo8AEfuzr0PT2ZEOds"
+
+var searchTerm = $('#search-term').val();
+var startYear = $('#start-year').val();
+var endYear = $('#end-year').val();
+var search = $('#run-search').val();
+var clearResults = $('#clear-all').val();
 
 
+
+$('#run-search').on('click', function () {
+    event.preventDefault()
+    var search = $('#search-term').val();
+})
 
 
 
