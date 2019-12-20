625d8d90d2328744be170c92e879828d8418cdb9
Robert Bovee <Rjbovee112@gmail.com>
2019-12-19 21:06:14 -0700

    working

--- NYT.js
+++ NYT.js
@@ -1,5 +1,5 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=IpG44rHfGTHu1tDo8AEfuzr0PT2ZEOds"
-var apiKey = "IpG44rHfGTHu1tDo8AEfuzr0PT2ZEOds"
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=IpG44rHfGTHu1tDo8AEfuzr0PT2ZEOds"
+var apiKey = "IIpG44rHfGTHu1tDo8AEfuzr0PT2ZEOds"
 
 var searchTerm = $('#search-term').val();
 var startYear = $('#start-year').val();
@@ -13,7 +13,7 @@ function clear() {
 
 
 function buildQueryURL() {
-    var myKey = "IpG44rHfGTHu1tDo8AEfuzr0PT2ZEOds"
+    var myKey = "IIpG44rHfGTHu1tDo8AEfuzr0PT2ZEOds"
     var searchTerm = $('#search-term').val();
     var startYear = $('#start-year').val();
     var endYear = $('#end-year').val();
@@ -23,18 +23,31 @@ function buildQueryURL() {
     var url = nytimes + "q=" + searchTerm + "begin_date=" + startYear + "&end_date=" + endYear + "&api_keys=" + myKey;
     console.log(url)
     return url;
+
+    if (parseInt(startYear)) {
+        startYear = startYear + "0101";
+    }
+
+    if (parsenInt(endYear)) {
+        endYear = endYear + "0101";
+    }
 }
+
+
 
 $('#run-search').on('click', function () {
     event.preventDefault()
-    var url = buildQueryURL()
-    console.log(url)
-})
+    clear();
+    var queryUrl = buildQueryURL()
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+    });
+});
 
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-})
+
+
